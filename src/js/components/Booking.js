802772475e6b7e64c11d3d106cc63ca4fc83ac84
Dominik Wilk <mr.wilkd@gmail.com>
2023-08-15 21:41:23 +0100

    Use plugins for pick date and time of booking

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,7 +1,8 @@
 import { select, templates } from '../settings.js';
 import utils from '../utils.js';
 import AmountWidget from './AmountWidget.js';
-
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 class Booking {
   constructor(element) {
     this.render(element);
@@ -17,12 +18,20 @@ class Booking {
     this.dom.peopleAmount = this.element.querySelector(select.booking.peopleAmount);
     this.dom.hoursAmount = this.element.querySelector(select.booking.hoursAmount);
 
+    this.dom.datePicker = this.element.querySelector(select.widgets.datePicker.wrapper);
+    this.dom.hourPicker = this.element.querySelector(select.widgets.hourPicker.wrapper);
+
     this.dom.wrapper.appendChild(this.element);
   }
 
   initWidgets() {
     this.peopleWidget = new AmountWidget(this.dom.peopleAmount);
     this.hoursWidget = new AmountWidget(this.dom.hoursAmount);
+    this.dateWidget = new DatePicker(this.dom.datePicker);
+    this.timeWidget = new HourPicker(this.dom.hourPicker);
+
+    this.dom.datePicker.addEventListener('updated', () => {});
+    this.dom.hourPicker.addEventListener('updated', () => {});
 
     this.dom.peopleAmount.addEventListener('updated', () => {});
 
