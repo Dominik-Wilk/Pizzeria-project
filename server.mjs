8e1ddd2ca924ec1521fcd32819ede1e5b51c55d7
Dominik Wilk <mr.wilkd@gmail.com>
2024-01-09 10:54:58 +0100

    update files for publication

--- server.mjs
+++ server.mjs
@@ -3,11 +3,8 @@ import path from 'path';
 import jsonServer from 'json-server';
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join('dist', 'db', 'app.json'));
-const middlewares = jsonServer.defaults({
-  static: 'dist',
-  noCors: true,
-});
+const router = jsonServer.router(path.join('src', 'db', 'app.json'));
+const middlewares = jsonServer.defaults({});
 const port = process.env.PORT || 3131;
 
 server.use(middlewares);
